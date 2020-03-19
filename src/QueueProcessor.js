d0b1e62a63b7cf2227bdec48e7b4624e99d0dc29
anchal04 <anchalsrivastava131271cse@gmail.com>
2020-03-19 09:49:14 +0530

    Added queueProcessor

---
+++ src/QueueProcessor.js
@@ -0,0 +1,59 @@
+/**
+ * Queue processor will handle all the queue related functions
+ */
+export default class QueueProcessor {
+    /**
+     * Initialize queue
+     */
+    constructor() {
+        this.queue = null;
+        this.currentJob = null;
+    }
+
+    /**
+     * Starting point of queue processing
+     */
+    start(queueObj) {
+        this.queue = queueObj;
+        this.currentJob = !this.queue.isEmpty() ? this.queue.peek() : null;
+        if (!this.queue.isEmpty()) {
+            this.processJob();
+        }
+    }
+
+    /**
+     * Execute the current job
+     */
+    processJob() {
+        if (this.currentJob) {
+            this.currentJob.execute(
+                this.onJobSuccess.bind(this),
+                this.onJobFail.bind(this),
+            );
+        }
+    }
+
+    /**
+     * Define job success behaviour
+     */
+    onJobSuccess() {
+        this.queue.dequeue();
+        if (this.queue.isEmpty()) {
+            this.currentJob = null;
+            return;
+        }
+        this.currentJob = this.queue.peek();
+        this.processJob();
+    }
+
+    /**
+     * Handle on failure event for job execution
+     */
+    onJobFail(response) {
+        this.queue.dequeue();
+        // eslint-disable-next-line no-console
+        console.error('error', response);
+    }
+}
+
+
