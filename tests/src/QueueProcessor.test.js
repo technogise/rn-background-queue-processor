8d405b1cfe8b185a3a7942ffb7db3459a7d6c96e
anchal04 <anchalsrivastava131271cse@gmail.com>
2020-03-20 14:55:20 +0530

    added test for queue processor

--- tests/src/QueueProcessor.test.js
+++ tests/src/QueueProcessor.test.js
@@ -5,26 +5,78 @@ import Job from "../../src/Job";
 
 describe('Test QueueProcessor', () => {
 
-    test('should start processing queue when invoked start',() => {
-        const response = { errors: 'ERROR' };
-        const jobSuccessCallback = (onSuccess, onFail) => {
-            onFail(response);
+    test('should start processing queue when invoked start',(done) => {
+        const jobSuccessCallback = (onSuccess) => {
+            onSuccess();
         };
-        const mockedAllJobs = jest.fn();
-        const jobToBeCreated = {
-            id: 123,
-            name: 'testJob',
+        const jobToBeCreated1 = {
+            name: 'testJob1',
             param: {},
         };
-        const job = new Job(jobToBeCreated);
-        mockedAllJobs.mockReturnValue({});
-        job.execute = jest.fn(jobSuccessCallback);
+        const jobToBeCreated2 = {
+            name: 'testJob2',
+            param: {},
+        };
+        const job1 = new Job(jobToBeCreated1);
+        const job2 = new Job(jobToBeCreated2);
+        job1.execute = jest.fn(jobSuccessCallback);
+        job2.execute = jest.fn(jobSuccessCallback);
         const dbAdapter = new InMemoryAdapter();
         const queueObj = new Queue(dbAdapter);
-        queueObj.enqueue(job);
+        queueObj.enqueue(job1);
+        queueObj.enqueue(job2);
         const queueProcessor = new QueueProcessor();
         queueProcessor.start(queueObj);
-        expect(queueProcessor.queue.getSize()).toBe(1);
+        setTimeout(() => {
+            expect(job1.execute).toHaveBeenCalled();
+            expect(job2.execute).toHaveBeenCalled();
+            expect(queueObj.getSize()).toBe(0);
+            done();
+        });
     });
 
+    it('should not start processing queue when no jobs found', (done) => {
+        const dbAdapter = new InMemoryAdapter();
+        const queueObj = new Queue(dbAdapter);
+        const queueProcessor = new QueueProcessor();
+        queueProcessor.start(queueObj);
+        setTimeout(() => {
+            expect(queueObj.getSize()).toBe(0);
+            done();
+        });
+    });
+
+    test('should dequeue and log error if any job fails',(done) => {
+        const response = "JOB_FAIL";
+        const jobFailCallback = (onSuccess, onFail) => {
+            onFail(response);
+        };
+        const jobSuccessCallback = (onSuccess) => {
+            onSuccess();
+        };
+        const jobToBeCreated1 = {
+            name: 'testJob1',
+            param: {},
+        };
+        const jobToBeCreated2 = {
+            name: 'testJob2',
+            param: {},
+        };
+        const job1 = new Job(jobToBeCreated1);
+        const job2 = new Job(jobToBeCreated2);
+        job1.execute = jest.fn(jobSuccessCallback);
+        job2.execute = jest.fn(jobFailCallback);
+        const dbAdapter = new InMemoryAdapter();
+        const queueObj = new Queue(dbAdapter);
+        queueObj.enqueue(job1);
+        queueObj.enqueue(job2);
+        const queueProcessor = new QueueProcessor();
+        queueProcessor.start(queueObj);
+        setTimeout(() => {
+            expect(job1.execute).toHaveBeenCalled();
+            expect(job2.execute).toHaveBeenCalled();
+            expect(queueObj.getSize()).toBe(0);
+            done();
+        });
+    });
 });
