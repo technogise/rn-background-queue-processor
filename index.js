5d9a3ee3cd5aaddadc9a401ef82b6efef78dfd30
anchal04 <anchalsrivastava131271cse@gmail.com>
2020-03-23 11:33:25 +0530

    added index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+import InMemoryAdapter from './src/DbAdapter/InMemoryAdapter'
+import Queue from './src/Queue';
+import Job from './src/Job';
+import Worker from './src/Worker';
+
+export {
+    InMemoryAdapter,
+    Queue,
+    Job,
+    Worker
+};
