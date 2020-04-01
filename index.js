853252368cb79c4efe24f4a0f3d6f0381162f1cb
anchal04 <anchalsrivastava131271cse@gmail.com>
2020-04-01 20:35:37 +0530

    added RealmAdapter in index file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import InMemoryAdapter from './src/DbAdapter/InMemoryAdapter'
+import RealmAdapter from "./src/DbAdapter/RealmAdapter";
 import Queue from './src/Queue';
 import Job from './src/Job';
 import Worker from './src/Worker';
@@ -6,6 +7,7 @@ import {maxRetries, retryInterval} from "./config/defaultConfigs";
 
 export {
     InMemoryAdapter,
+    RealmAdapter,
     Queue,
     Job,
     Worker,
