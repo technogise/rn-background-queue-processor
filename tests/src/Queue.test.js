2b339249123a601fd0d7a95283e0f212b19b94ea
anchal04 <anchalsrivastava131271cse@gmail.com>
2020-04-01 13:34:35 +0530

    fixed queue tests

--- tests/src/Queue.test.js
+++ tests/src/Queue.test.js
@@ -25,23 +25,59 @@ describe('Test allQueues file', () => {
     });
 
     test('should test isEmpty to be falsy', () => {
+        const jobToBeCreated1 = {
+            id: 11,
+            name: 'testJob1',
+            param: {},
+        };
+        const jobToBeCreated2 = {
+            id: 22,
+            name: 'testJob2',
+            param: {},
+        };
+        const jobToBeCreated3 = {
+            id: 33,
+            name: 'testJob3',
+            param: {},
+        };
+        const job1 = new Job(jobToBeCreated1);
+        const job2 = new Job(jobToBeCreated2);
+        const job3 = new Job(jobToBeCreated3);
         const dbAdapter = new InMemoryAdapter();
         const queue = new Queue(dbAdapter);
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
+        queue.enqueue(job1);
+        queue.enqueue(job2);
+        queue.enqueue(job3);
         const actual = queue.isEmpty();
         expect(actual).toBeFalsy();
     });
 
     test('should test peek', () => {
+        const jobToBeCreated1 = {
+            id: 11,
+            name: 'testJob1',
+            param: {},
+        };
+        const jobToBeCreated2 = {
+            id: 22,
+            name: 'testJob2',
+            param: {},
+        };
+        const jobToBeCreated3 = {
+            id: 33,
+            name: 'testJob3',
+            param: {},
+        };
+        const job1 = new Job(jobToBeCreated1);
+        const job2 = new Job(jobToBeCreated2);
+        const job3 = new Job(jobToBeCreated3);
         const dbAdapter = new InMemoryAdapter();
         const queue = new Queue(dbAdapter);
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
+        queue.enqueue(job1);
+        queue.enqueue(job2);
+        queue.enqueue(job3);
         const actual = queue.peek();
-        expect(actual).toBe(1);
+        expect(actual).toBe(job1);
     });
 
     test('should test dequeue', () => {
@@ -74,14 +110,32 @@ describe('Test allQueues file', () => {
     });
 
     test('should test enqueue', () => {
+        const jobToBeCreated1 = {
+            id: 11,
+            name: 'testJob1',
+            param: {},
+        };
+        const jobToBeCreated2 = {
+            id: 22,
+            name: 'testJob2',
+            param: {},
+        };
+        const jobToBeCreated3 = {
+            id: 33,
+            name: 'testJob3',
+            param: {},
+        };
+        const job1 = new Job(jobToBeCreated1);
+        const job2 = new Job(jobToBeCreated2);
+        const job3 = new Job(jobToBeCreated3);
         const dbAdapter = new InMemoryAdapter();
         const queue = new Queue(dbAdapter);
-        queue.enqueue(12);
-        queue.enqueue(22);
-        queue.enqueue(32);
+        queue.enqueue(job1);
+        queue.enqueue(job2);
+        queue.enqueue(job3);
         const actual = queue.getItems();
-        expect(actual).toContain(12);
-        expect(actual).toContain(22);
-        expect(actual).toContain(32);
+        expect(actual).toContain(job1);
+        expect(actual).toContain(job2);
+        expect(actual).toContain(job3);
     });
 });
