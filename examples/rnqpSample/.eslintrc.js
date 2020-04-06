9332331269af76b294ec0ca1322110d3ab57e186
anchal04 <anchalsrivastava131271cse@gmail.com>
2020-04-06 11:05:07 +0530

    added eslintrc

--- examples/rnqpSample/.eslintrc.js
+++ examples/rnqpSample/.eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  "parser": "babel-eslint",
+  "extends": "airbnb",
+  "plugins": ["react", "jsx-a11y", "import"],
+  "rules": {
+    "prefer-destructuring": ["error", {"object": true, "array": false}],
+    "react/jsx-filename-extension": ["off"],
+    "linebreak-style": ["off"],
+    "no-undef": ["error"],
+    "react/sort-comp": ["off"],
+    "react/prefer-stateless-function": ["off"],
+    "react/destructuring-assignment": ["off"],
+    "react/prop-types": ["off"],
+    "import/prefer-default-export": ["off"],
+    "class-methods-use-this": ["off"],
+    "react/no-access-state-in-setstate": ["off"],
+    "react/no-array-index-key": ["off"],
+  },
+  "globals": {
+    "it": true,
+    "expect": true,
+    "describe": true,
+    "navigator": true,
+    "fetch": true,
+    "jest": true,
+    "test": true,
+  }
 };
