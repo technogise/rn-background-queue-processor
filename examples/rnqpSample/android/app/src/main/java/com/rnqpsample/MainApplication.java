1a94242789d3b7b16f986a81e5fced049b1d2fce
varunshukla <varunshukla415@gmail.com>
2020-04-07 20:02:10 +0530

    app gradle update and maven add

--- examples/rnqpSample/android/app/src/main/java/com/rnqpsample/MainApplication.java
+++ examples/rnqpSample/android/app/src/main/java/com/rnqpsample/MainApplication.java
@@ -2,6 +2,8 @@ package com.rnqpsample;
 
 import android.app.Application;
 import android.content.Context;
+
+import com.facebook.react.BuildConfig;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
